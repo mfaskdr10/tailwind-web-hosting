8c41c0964bfb08baff9685d615dc68d71393ec96
Fadhil Alvan <mfaskdr10@gmail.com>
2023-11-20 18:43:26 +0700

    menambahkan-navbar

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: ["./src/**/*.{html, js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#F1F4F5",
+      },
+    },
   },
   plugins: [],
 };
